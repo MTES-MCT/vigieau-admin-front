dfea0f785704f601b3d81ce29ab05ccbe7d45da9
Vincent Lainé <vincent.laine.utc@gmail.com>
2023-11-27 15:19:12 +0100

    Init projet DSFR ✨

---
+++ client/pages/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'basic',
+})
+</script>
+
+<template>
+  <div>
+    <Html lang="fr">
+      <Head>
+        <Title>Accueil - Gabarit de démarrage VueDsfr</Title>
+      </Head>
+    </Html>
+
+    <h1 class="fr-mt-4w">
+      Accueil
+    </h1>
+  </div>
+</template>
