6861008ff56487f251cbaa0d18f89464121cf7ca
Vincent Lainé <vincent.laine.utc@gmail.com>
2023-12-04 18:56:18 +0100

    Rajout de la page d'erreur ✨

---
+++ client/error.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+useHead({
+  title: `Erreur - ${useRuntimeConfig().public.appName}`,
+})
+
+const props = defineProps<{
+  error: any
+}>()
+const router = useRouter()
+
+const title = computed<string>(() => {
+  return props.error.statusCode === 404 ? 'Page non trouvée, ne paniquez pas' : 'Une erreur est survenue, ne paniquez pas'
+})
+const subtitle = computed<string>(() => {
+  return `Erreur ${props.error.statusCode} !`
+})
+const buttons = [
+  {
+    label: 'Page d\'accueil',
+    onClick: () => {
+      router.push('/')
+    },
+  },
+]
+</script>
+
+<template>
+  <div>
+    <NuxtLayout name="basic">
+      <DsfrErrorPage
+        class="fr-mt-8w"
+        :title="title"
+        :subtitle="subtitle"
+        description=""
+        help=""
+        :buttons="buttons"
+      />
+    </NuxtLayout>
+  </div>
+</template>
